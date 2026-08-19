c96ae581d5eaba42dfe40538c954cab951bc5493
Ashutosh Maurya <ashutoshm9695@gmail.com>
2026-08-19 10:24:50 +0530

    fix: shared db fix

--- test_app.py
+++ test_app.py
@@ -19,9 +19,9 @@ def client():
         })
     yield client
 
-    # Teardown: drop DB after test
+    # Teardown: clear test data
     with app.app_context():
-        mongo.cx.drop_database("test_student_db")
+        mongo.db.students.delete_many({})
 
 
 def test_home_page(client):
